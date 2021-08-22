1d0f1fd198822f25559fd325afd7e0bed12cb18c
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2021-08-22 23:58:45 +0100

    replaced dictionary with list (100%)

--- PE#14.py
+++ PE#14.py
@@ -2,20 +2,6 @@
 # by storing the results of expensive function calls and returning the cached result
 # when the same inputs occur again.
 
-nr_steps = {1:0}
-
-def collatz(x):
-    if x in nr_steps:
-        return nr_steps[x]
-
-    if x % 2 == 0:
-        x_step = 1 + collatz(x//2)
-    else:
-        x_step = 1 + collatz(3*x+1)
-    # store result globally
-    nr_steps[x] = x_step
-    return x_step
-
 t = int(input().strip())
 n_list = []
 for a0 in range(t):
@@ -23,13 +9,31 @@ for a0 in range(t):
     n_list.append(n)
 n_max = max(n_list)
 
+nr_steps = [0]*n_max
+
+def collatz(x):
+    if x == 1:
+        return 0
+    if x <= n_max:
+        if nr_steps[x-1]:
+            return nr_steps[x-1]
+
+    if x % 2 == 0:
+        x_step = 1 + collatz(x//2)
+    else:
+        x_step = 1 + collatz(3*x+1)
+
+    if x <= n_max:
+        # store result globally
+        nr_steps[x-1] = x_step
+    return x_step
+
 for i in range(1,n_max+1):
-    if i not in nr_steps:
+    if not nr_steps[i-1]:
         collatz(i)
 
-all_steps = list({ your_key: nr_steps[your_key] for your_key in range(1,n_max+1) }.values())
 results = [(0,1)]
-for x,x_step in enumerate(all_steps[1:]):
+for x,x_step in enumerate(nr_steps[1:]):
     results.append(max(results[-1] , (x_step,x+2)))
 
 for n in n_list:
