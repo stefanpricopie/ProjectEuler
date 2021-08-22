89efeaee50619a1fc6966cb7bf281f3a2b6650f8
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2021-08-22 22:50:29 +0100

    new solution 40%

--- PE#14.py
+++ PE#14.py
@@ -1,24 +1,31 @@
-def rev_collatz(x, x_steps, n):
-    global x_best, most_steps, not_evaluated
-    print(not_evaluated)
-    if x <= n:
-        not_evaluated -= 1
-        if x_steps > most_steps:
-            x_best, most_steps = x, x_steps
-    print('x = {}, steps = {}'.format(x,x_steps))
-    if not_evaluated:
-        # if x is even and 3k+1, then it can be the next step for either k (odd) or for 2x
-        if x % 6 == 4 and x>4: 
-            rev_collatz((x-1)//3, x_steps+1, n)
-        
-        if x % 3 != 0 or x * 2 <= n:
-            rev_collatz(2*x, x_steps+1, n)    
+def collatz(x):
+    global nr_steps
+    if x in nr_steps:
+        return nr_steps[x]
+    else:
+        if x % 2 == 0:
+            x_step = 1 + collatz(x//2)
+        else:
+            x_step = 1 + collatz(3*x+1)
+        # store result globally
+        nr_steps[x] = x_step
+        return x_step
 
 t = int(input().strip())
+n_list = []
 for a0 in range(t):
     n = int(input().strip())
-    x_best = 1
-    most_steps = 0
-    not_evaluated = n
-    rev_collatz(1, 0, n)
-    print(x_best, most_steps)
+    n_list.append(n)
+n_max = max(n_list)
+
+nr_steps = {1: 0}
+for i in range(1,n_max+1):
+    if i not in nr_steps:
+        collatz(i)
+
+results = list({ your_key: nr_steps[your_key] for your_key in range(1,n_max+1) }.values())
+results.reverse()
+
+for n in n_list:
+    slice = results[-n:]
+    print(n - slice.index(max(slice)))
