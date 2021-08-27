bc73c577b65dc2bb95b7c20225f3766047a9f8e0
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2021-08-27 01:32:56 +0100

    100% (used 28123)

--- PE#23.py
+++ PE#23.py
@@ -1,7 +1,7 @@
 t = int(input())
 n_list = []
 [n_list.append(int(input())) for _ in range(t)]
-n_max = max(n_list)
+n_max = min(max(n_list),28123)
 
 # Sieve of Eratosthenes
 primes = []
@@ -14,18 +14,20 @@ for i in range(2,n_max+1):
         for j in range(i*i,n_max+1,i):
             is_prime[j]=False
 
-def fast_sum_of_proper_divisors(nr):
+def abundant_sum_of_proper_divisors(nr):
     """
     input n, a list of primes, and a list of booleans
     returns the sum of the proper divisors
     """
     if is_prime[nr]:
-        return 1
+        return False
     nr_copy = nr
     product = 1
     for p in primes:
         if is_prime[nr_copy]:
             product *= (nr_copy**(1 + 1) - 1)//(nr_copy - 1)
+            if product-nr > nr:
+                return True
             break
         if p > nr_copy:
             break
@@ -35,12 +37,14 @@ def fast_sum_of_proper_divisors(nr):
             i += 1
         if i != 0:
             product *= (p**(i + 1) - 1)//(p - 1)
-    return product - nr
+            if product-nr > nr:
+                return True
+    return False
 
 abundants = []
 sum_abundants = [False]*(n_max+1)
 for nr in range(2, n_max+1):
-    if fast_sum_of_proper_divisors(nr) > nr:
+    if abundant_sum_of_proper_divisors(nr):
         abundants.append(nr)
         for a in abundants:
             if n_max < nr + a:
@@ -48,7 +52,9 @@ for nr in range(2, n_max+1):
             sum_abundants[nr+a] = True
 
 for n in n_list:
-    if sum_abundants[n]:
+    if n > 28123:
+        print('YES')
+    elif sum_abundants[n]:
         print('YES')
     else:
         print('NO')
