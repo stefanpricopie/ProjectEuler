727d64b085c93338ce9de1839cb8aa6eeebd095d
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2022-11-20 15:08:18 +0000

    Remove ignored files

--- pe15/pe15.py
+++
@@ -1,9 +0,0 @@
-from math import factorial as fact
-
-def combinatorial(n,k):
-    return (fact(n) // (fact(k) * fact(n-k))) % 1000000007
-
-t = int(input().strip())
-for a0 in range(t):
-    m,n = [int(i) for i in input().strip().split(' ')]
-    print(combinatorial(m+n,m))
